6ea8b620d2a5e3529ff0e18d1051f306b985f8b7
Олег Матшин <89175955650@mail.ru>
2022-03-10 18:54:18 +0300

    New method put_rating

--- HomeTask_2_100322.py
+++ HomeTask_2_100322.py
@@ -8,22 +8,13 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-# Old second class Mentor         
+# Old second class Mentor now without the method rate_hw
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
         
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
-
 # New subclass from parent Mentor with initialisation
 class Lecturer(Mentor):
     def __init__(self, name, surname):
@@ -34,17 +25,28 @@ class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
 
-# Block with old code
+# New method put_rating
+    def put_rating(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return "It's Mistake!"
+
+# Block of tests
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
  
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.put_rating(best_student, 'Python', 10)
+cool_reviewer.put_rating(best_student, 'Python', 10)
+cool_reviewer.put_rating(best_student, 'Python', 10)
  
 print(best_student.grades)
 print(best_student.name, best_student.surname, best_student.gender)
 print(best_student.courses_in_progress, best_student.finished_courses)
+print(cool_reviewer.name, cool_reviewer.surname, cool_reviewer.courses_attached)
